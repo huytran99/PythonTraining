12099ffbb0e3716c9c37a61196e465a5f8ebb360
Hieu Nguyen <harry@Hieus-Air.lan>
2024-10-25 18:18:46 +0700

    Vi du ve git commit, code mau quan_ly_sinh_vien

---
+++ HieuNguyen/quan_ly_sinh_vien.py
@@ -0,0 +1,11 @@
+print("Chào mừng bạn đến với hệ thống quản lý sinh viên")
+def hien_thi_menu():
+    print("\n1. Thêm sinh viên")
+    print("2. Hiển thị danh sách sinh viên")
+    print("3. Tính điểm trung bình của sinh viên")
+    print("4. Tìm kiếm sinh viên")
+    print("5. Cập nhật thông tin sinh viên")
+    print("6. Xóa sinh viên")
+    print("7. Thoát chương trình")
+
+hien_thi_menu()
